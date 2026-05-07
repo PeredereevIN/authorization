eb41865f257ed069c4de2ed3a8fc9ce7380cb2ec
Передереев Илья Николаевич <peredereevin@gmail.com>
2026-05-07 04:53:39 +0300

    12345

--- backend/session-service/dist/router/session.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const session_module_1 = require("../module/session.module");
-const sessionRoutes = (0, express_1.Router)();
-sessionRoutes.get("/all", session_module_1.sessionController.getAllSession);
-sessionRoutes.get("/", session_module_1.sessionController.getSession);
-sessionRoutes.delete("/:id", session_module_1.sessionController.deleteSession);
-exports.default = sessionRoutes;
